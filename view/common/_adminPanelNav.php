130a303530f7a7d23a2afa7520b7959cd5735790
carolusoc <60094762+carolusoc@users.noreply.github.com>
2020-11-21 00:51:53 +0100

    Add files via upload

--- view/common/_adminPanelNav.php
+++ view/common/_adminPanelNav.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <a class="navbar-brand" href="#">Colegio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -12,40 +12,43 @@
         <li class="nav-item">
           <a class="nav-link" href="?pag=_asignaturasView.php">Asignaturas</a>
         </li>
-         <li class="nav-item">
-          <a class="nav-link" href="?pag=_cursosView.php">Cursos</a>
+      
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Cursos</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="?pag=_cursosView.php">Edición Cursos</a>
+              <a class="dropdown-item" href="?pag=_asigxcursoView.php">Asignaturas por Curso</a>
+            </div>
         </li>
-    
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profesorado</a>
-              <div class="dropdown-menu">
+            <div class="dropdown-menu">
               <a class="dropdown-item" href="?pag=_profesoresView.php">Alta Profesor</a>
-            
               <a class="dropdown-item" href="?pag=_profeAsignaView.php">Asignar Asignaturas</a>
             </div>
         </li>
-        
-        
-        
-        
-        
-         <li class="nav-item">
+       <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Alumnos</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="?pag=_adminAlumnView.php">Alta Alumno</a>
+                <a class="dropdown-item" href="?pag=_adminMatriculasView.php">Matricular Alumno</a>
+              </div>
+        </li>
+      
+        <li class="nav-item">
           <a class="nav-link" href="?pag=_calendarioView.php">Calendario</a>
         </li>
-     
-        
-        
         <li class="nav-item">
           <a class="nav-link" href="?pag=_userConfigView.php">Configuración</a>
         </li>
-        
-           <li class="nav-item">
-          <a class="nav-link" href="?pag=salir">Salir</a>
+        <li class="nav-item pull-right">
+            <form action="" method="post" class="">
+                <input type="submit" class="nav-link salir" name="exit" value="Salir">
+            </form>
+          
         </li>
-        
-      </ul>
-      
-    </div>
+        </ul>
+      </div>
   </nav>
 <script>
    $('.dropdown-toggle').dropdown()
